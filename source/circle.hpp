cbdac5d19a7e010502f3cd2b5bd3ac7b232d3f4a
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-18 12:39:04 +0200

    create circle.hpp

---
+++ source/circle.hpp
@@ -0,0 +1,21 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "vec2.hpp"
+
+class Circle {
+public:
+    Circle(Vec2 const& ctr, float r);
+    float area() const;
+    float circumference() const;
+    float radius() const;
+    Vec2 center() const;
+
+private:
+    Vec2 center_;
+    float radius_ = 0.0f;
+
+
+};
+
+#endif // CIRCLE_HPP
