d30e50abd77c8bfeeffff256e8fbaeb14a6ac07b
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-18 12:40:19 +0200

    create rectangle.hpp

---
+++ source/rectangle.hpp
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "vec2.hpp"
+
+
+class Rectangle {
+
+public:
+    Rectangle(Vec2 const& min, Vec2 const& max);
+    float area() const;
+    float circumference() const;
+    Vec2 max() const;
+    Vec2 min() const;
+
+private:
+    Vec2 max_;
+    Vec2 min_;
+
+};
+
+#endif // RECTANGLE_HPP
