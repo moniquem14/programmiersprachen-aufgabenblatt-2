fa5f5a4bc8e078c63f4e30ba351cae3f3c86a03b
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-26 21:19:25 +0200

    draw()

--- source/circle.hpp
+++ source/circle.hpp
@@ -4,18 +4,37 @@
 
 class Circle {
 public:
+
+    Circle(Vec2 const& ctr, float r, Color const& clr);
+
     Circle(Vec2 const& ctr, float r);
+
     float area() const;
+
     float circumference() const;
+
+    Color color() const;
+
+    Vec2 ctr() const;
+
     float radius() const;
-    Vec2 center() const;
-   
+
+    bool is_inside(Vec2 const& p);
+
+    void draw(Window const& win) const;
+
+    void draw(Window const& win, Color const& color) const;
+
+    void draw(Window const& win, Color const& color, float thickness) const;
+
+    void draw(Window const& win, float thickness) const;
+
+
 
 private:
-    Vec2 center_;
-    float radius_ = 0.0f;
-    
-
+    Vec2 ctr_;
+    float radius_ = 0;
+    Color clr_;
 
 };
 
